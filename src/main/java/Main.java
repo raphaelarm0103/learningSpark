2501c1522123fa84d52fea7f6df2f1e6d56546f6
Raphael Marques <phaell_@hotmail.com>
2022-03-26 13:48:54 -0300

    Reduce by key java puro

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaRDDLike;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
+import scala.Tuple2;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -17,26 +17,31 @@ import java.util.List;
 public class Main implements Serializable {
 
     public static void main(String[] args) throws IOException {
-        List<Integer> inputData = new ArrayList<>();
+        List<String> inputData = new ArrayList<>();
 
         System.setProperty("hadoop.home.dir", "c:/hadoop");
-        inputData.add(35);
-        inputData.add(12);
-        inputData.add(90);
-        inputData.add(20);
+        inputData.add("WARN: Terça-Feira 12 de Abril de 2022");
+        inputData.add("ERROR: Quarta-Feira 16 de Maio de 2022");
+        inputData.add("WARN: Terça-Feira 25 de Abril de 2022");
+        inputData.add("ERROR: Quinta-Feira 12 de Junho de 2022");
+        inputData.add("FATAL: Sexta-Feira 12 de Maio de 2022");
 
-        Logger.getLogger("org.apa   che").setLevel(Level.WARN);
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf sparkConf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
-        Integer result = myRdd.reduce(Integer::sum);
+        JavaRDD<String> originalLogValues = sc.parallelize(inputData);
 
-        JavaRDD<Double> sqrtRdd = myRdd.map(Math::sqrt);
+        JavaPairRDD<String, Long> pairRDD = (originalLogValues.mapToPair(s -> {
+           String[] splitValues = s.split(":");
+           String levelLog = splitValues[0];
+           return new Tuple2<>(levelLog, 1L);
 
-        sqrtRdd.foreach(integer ->System.out.println(integer));
-        System.out.println(result);
+        }));
+
+        JavaPairRDD<String, Long> stringLongJavaPairRDD = pairRDD.reduceByKey((value1, value2) -> value1 + value2);
+        stringLongJavaPairRDD.foreach((tuple -> System.out.println(tuple._1 + "tem" + tuple._2 + "instancias")));
 
         sc.close();
 
